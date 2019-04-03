8c0890670ee95cf619f53b057f10ca6445649436
swbengs <swbengs@yahoo.com>
2019-04-03 14:30:56 -0500

    Added full resource file and a test one that is a fraction of the size Both contain minor code I added to help test things

---
+++ SWGResourceDatabase/table_test.lua
@@ -0,0 +1,100 @@
+--test file that doesn't have 300k lines of lua table code ;)
+resources = {
+	
+	{
+	name = "Ababuglu",
+
+	type = "copper_borocarbitic",
+		
+	classes = {
+
+		{"Inorganic", "inorganic"},
+
+		{"Mineral", "mineral"},
+	
+		{"Metal", "metal"},
+
+		{"Non-Ferrous Metal", "metal_nonferrous"},
+
+		{"Copper", "copper"},
+			
+		{"Conductive Borcarbitic Copper", "copper_borocarbitic"},
+
+		},
+
+
+	attributes = {
+
+		{"res_cold_resist", 184},
+		
+		{"res_conductivity", 539},
+
+		{"res_decay_resist", 906},
+			
+		{"res_heat_resist", 86},
+			
+		{"res_malleability", 349},
+			
+		{"res_quality", 469},
+			
+		{"res_shock_resistance", 344},
+			
+		{"res_toughness", 598},
+
+		},
+
+
+	zoneRestriction = "",
+
+	surveyToolType = 6,
+	containerCRC = 3684838486,
+
+	},
+
+
+
+	{
+
+	name = "Abayquily",
+
+	type = "bone_horn_rori",
+
+	classes = {
+
+		{"Organic", "organic"},
+
+		{"Creature Resources", "creature_resources"},
+
+		{"Creature Structural", "creature_structural"},
+	
+		{"Horn", "bone_horn"},
+
+		{"Rori Horn", "bone_horn_rori"},
+
+		},
+
+	attributes = {
+
+			{"res_decay_resist", 616},
+
+			{"res_malleability", 172},
+
+			{"res_quality", 424},
+
+			{"res_shock_resistance", 301},
+
+			{"res_toughness", 98},
+
+		},
+
+	zoneRestriction = "rori",
+	
+	surveyToolType = -1,
+
+	containerCRC = 1001781097,
+
+	}
+}
+
+--print("hello world");
+
