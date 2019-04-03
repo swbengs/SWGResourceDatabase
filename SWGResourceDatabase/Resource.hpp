a004dad42a342dd666dcaae0bfeba66e460018c0
swbengs <swbengs@yahoo.com>
2019-04-03 16:30:18 -0500

    Added resource class and resource pod

---
+++ SWGResourceDatabase/Resource.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+//std lib includes
+#include <string>
+
+//other includes
+#include "pods.hpp"
+
+/*
+MIT License
+
+Copyright (c) 2019 Scott Bengs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+Description: Class to store a generic SWG resource. 0 is treated as null for all resource attributes. Example: flavor = 0 is the same as having NULL in the database
+*/
+
+//class delcaration
+class Resource
+{
+public:
+    Resource();
+    Resource(const resource_pod& pod);
+private:
+    resource_pod resource;
+};
+
