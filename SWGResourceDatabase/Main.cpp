cd58f414e2bc9b1b71125cee3cb482225e8741a0
swbengs <swbengs@yahoo.com>
2019-04-04 00:13:56 -0500

    Tests added and all 10 keys work

--- SWGResourceDatabase/Main.cpp
+++ SWGResourceDatabase/Main.cpp
@@ -99,6 +99,8 @@ bool test_LuaCoreStop()
 
 bool test_LuaCoreFirst()
 {
+    printf("test_LuaCoreFirst start\n");
+
     std::string filename = "table_test.lua";
     LuaCore lua;
     resource_pod resource;
@@ -108,6 +110,7 @@ bool test_LuaCoreFirst()
     {
         return EXIT_FAILURE;
     }
+
     lua.getNextResource(resource);
     lua.stop();
 
@@ -173,6 +176,184 @@ bool test_LuaCoreFirst()
     printf("shock_resistance: %i\n", resource.shock_resistance);
     printf("unit_toughness: %i\n", resource.unit_toughness);
 
+    printf("test_LuaCoreFirst stop\n\n");
+
+    return EXIT_SUCCESS;
+}
+
+bool test_LuaCoreSecond()
+{
+    printf("test_LuaCoreSecond start\n");
+
+    std::string filename = "table_test.lua";
+    LuaCore lua;
+    resource_pod resource;
+    resetResourcePOD(resource);
+
+    if (!lua.start(filename))
+    {
+        return EXIT_FAILURE;
+    }
+
+    lua.getNextResource(resource); //1
+    resetResourcePOD(resource);
+    lua.getNextResource(resource); //2
+    lua.stop();
+
+    if (!checkString(resource.name, "Abayquily", "name"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkString(resource.type, "bone_horn_rori", "type"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.cold_resistance, 0, "cold_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.conductivity, 0, "conductivity"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.decay_resistance, 616, "decay_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.flavor, 0, "flavor"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.heat_resistance, 0, "heat_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.malleability, 172, "malleability"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.overall_quality, 424, "overall_quality"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.potential_energy, 0, "potential_energy"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.shock_resistance, 301, "shock_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.unit_toughness, 98, "unit_toughness"))
+    {
+        return EXIT_FAILURE;
+    }
+
+    printf("name: %s\n", resource.name.c_str());
+    printf("type: %s\n", resource.type.c_str());
+    printf("cold_resistance: %i\n", resource.cold_resistance);
+    printf("conductivity: %i\n", resource.conductivity);
+    printf("decay_resistance: %i\n", resource.decay_resistance);
+    printf("flavor: %i\n", resource.flavor);
+    printf("heat_resistance: %i\n", resource.heat_resistance);
+    printf("malleability: %i\n", resource.malleability);
+    printf("overall_quality: %i\n", resource.overall_quality);
+    printf("potential_energy: %i\n", resource.potential_energy);
+    printf("shock_resistance: %i\n", resource.shock_resistance);
+    printf("unit_toughness: %i\n", resource.unit_toughness);
+
+    printf("test_LuaCoreSecond stop\n\n");
+
+    return EXIT_SUCCESS;
+}
+
+bool test_LuaCoreFourth()
+{
+    printf("test_LuaCoreFourth start\n");
+
+    std::string filename = "table_test.lua";
+    LuaCore lua;
+    resource_pod resource;
+    resetResourcePOD(resource);
+
+    if (!lua.start(filename))
+    {
+        return EXIT_FAILURE;
+    }
+
+    lua.getNextResource(resource); //1
+    resetResourcePOD(resource);
+    lua.getNextResource(resource); //2
+    resetResourcePOD(resource);
+    lua.getNextResource(resource); //3
+    resetResourcePOD(resource);
+    lua.getNextResource(resource); //4
+    lua.stop();
+
+    if (!checkString(resource.name, "Able", "name"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkString(resource.type, "fruit_fruits_dantooine", "type"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.cold_resistance, 0, "cold_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.conductivity, 0, "conductivity"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.decay_resistance, 828, "decay_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.flavor, 626, "flavor"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.heat_resistance, 0, "heat_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.malleability, 0, "malleability"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.overall_quality, 523, "overall_quality"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.potential_energy, 182, "potential_energy"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.shock_resistance, 0, "shock_resistance"))
+    {
+        return EXIT_FAILURE;
+    }
+    else if (!checkInt(resource.unit_toughness, 0, "unit_toughness"))
+    {
+        return EXIT_FAILURE;
+    }
+
+    printf("name: %s\n", resource.name.c_str());
+    printf("type: %s\n", resource.type.c_str());
+    printf("cold_resistance: %i\n", resource.cold_resistance);
+    printf("conductivity: %i\n", resource.conductivity);
+    printf("decay_resistance: %i\n", resource.decay_resistance);
+    printf("flavor: %i\n", resource.flavor);
+    printf("heat_resistance: %i\n", resource.heat_resistance);
+    printf("malleability: %i\n", resource.malleability);
+    printf("overall_quality: %i\n", resource.overall_quality);
+    printf("potential_energy: %i\n", resource.potential_energy);
+    printf("shock_resistance: %i\n", resource.shock_resistance);
+    printf("unit_toughness: %i\n", resource.unit_toughness);
+
+    printf("test_LuaCoreFourth stop\n\n");
+
     return EXIT_SUCCESS;
 }
 
@@ -183,6 +364,8 @@ int main()
     //result = test_LuaCoreBasic();
     //result = test_LuaCoreStop();
     result = test_LuaCoreFirst();
+    result = test_LuaCoreSecond();
+    result = test_LuaCoreFourth();
 
     return result;
 }
